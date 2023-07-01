d431ce1e517fa0e927bd26d0ef8b35af9391dca8
Epsilon <134446210+Epsilon538@users.noreply.github.com>
2023-07-01 05:55:36 -0400

    jttu6

--- app/Http/Controllers/ArtistaController.php
+++ app/Http/Controllers/ArtistaController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 use App\Models\Perfil;
+use App\Models\Imagen;
 use Illuminate\Http\Request;
 
 class ArtistaController extends Controller
 {
     public function perfil(){
         return view('artista.perfil');
+    }
+
+    public function galeria(){
+        $imagenes = Imagen::all();
+        return view('artista.vista_artista',compact('imagenes'));
     }
 }
