f830f56f2de8e413fb0f7d829cc6115fe717cde8
Epsilon <134446210+Epsilon538@users.noreply.github.com>
2023-07-01 03:48:04 -0400

    Update perfil.blade.php

--- resources/views/Artista/perfil.blade.php
+++ resources/views/Artista/perfil.blade.php
@@ -10,6 +10,12 @@
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
+                    <div class="card-body">
+                        <form action="..." method="POST">
+                            <div><input type="file" name="archivo" id="archivo" class="form-control"></div>
+                            <div class="mt-3"><button type="submit" class="btn btn-secondary-rojo">Subir propuesta</button></div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
