5a7861aa1434db5c5da651329b6870ae386d165f
Bastian <bastian.rojash@usm.cl>
2023-06-30 22:53:39 -0400

    seeder y nada mas creo
    :3

---
+++ database/seeders/PerfilSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PerfilSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('perfiles')->insert([
+            ['nombre'=>'admin'],['nombre'=>'artista'] 
+        ]);
+    }
+}
